fa85e3a2cde586263e9fc8ca0803fb3b10b58498
looken1024 <looken1024@126.com>
2025-04-30 15:07:35 +0800

    feat:同步源码

---
+++ depends.mk
@@ -0,0 +1,3 @@
+BOOST_HEADER_DIR=/home/yangkai/github/sofa-pbrpc/thirdparty/boost_1_57_0
+PROTOBUF_DIR=/home/yangkai/github/sofa-pbrpc/thirdparty
+SNAPPY_DIR=/home/yangkai/github/sofa-pbrpc/thirdparty
